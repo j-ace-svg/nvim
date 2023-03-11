f11cfd7e02efc714623c182d23489412d9047481
John Montgomery <j.ace.svg@gmail.com>
2023-03-11 15:36:52 -0600

    [FEAT] Add working Telescope mappings for file and git repo search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,12 @@ local Remap = require('j-ace-svg.keymap')
 local nnrm = Remap.nnoremap
 local builtin = require('telescope.builtin')
 
-nnrm('<Leader>pf', builtin.find_files)
+nnrm('<Leader>pf', function()
+    builtin.find_files({ hidden = true, respect_gitignore = true });
+end)
+nnrm('<Leader>pg', function()
+    builtin.git_files({ hidden = true, respect_gitignore = true });
+end)
 nnrm("<Leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep For > ") });
 end)
