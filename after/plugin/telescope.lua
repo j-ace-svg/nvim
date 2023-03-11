c475b8ce449b8d82923455a1786183a93475f1da
John Montgomery <j.ace.svg@gmail.com>
2023-03-11 16:24:13 -0600

    [FEAT] Make telescope search relative to buffer location

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,10 @@
 local Remap = require('j-ace-svg.keymap')
 local nnrm = Remap.nnoremap
 local builtin = require('telescope.builtin')
+local utils = require('telescope.utils')
 
 nnrm('<Leader>pf', function()
-    builtin.find_files({ hidden = true, respect_gitignore = true });
+    builtin.find_files({ hidden = true, respect_gitignore = true, cwd = utils.buffer_dir() });
 end)
 nnrm('<Leader>pg', function()
     builtin.git_files({ hidden = true, respect_gitignore = true });
