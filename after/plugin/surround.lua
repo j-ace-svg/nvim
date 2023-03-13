9bb07e7ab308bc9c74ba3ead234345ae3e8c50eb
John Montgomery <j.ace.svg@gmail.com>
2023-03-13 04:06:58 -0500

    [FEAT] Extend smart bracket/brace shortcuts to command mode

--- after/plugin/surround.lua
+++ after/plugin/surround.lua
@@ -1,6 +1,7 @@
 local Remap = require('j-ace-svg.keymap')
 local nnrm = Remap.nnoremap
 local inrm = Remap.inoremap
+local cnrm = Remap.cnoremap
 local surround = require('nvim-surround')
 local config = require('nvim-surround.config')
 
@@ -12,7 +13,7 @@ inrm("<C-s>", "<C-g>u<Plug>(nvim-surround-insert)")
 inrm("<C-s><C-s>", "<C-g>u<Plug>(nvim-surround-insert-line)")
 
 -- Quick access auto-surrounds
-local surround_fun = function(opening, closing)
+local surround_fun_ins = function(opening, closing)
     return function()
         local win = vim.api.nvim_get_current_win()
         local byte_index = vim.api.nvim_win_get_cursor(win)[2]
@@ -25,6 +26,22 @@ local surround_fun = function(opening, closing)
     end
 end
 
-inrm("<C-b>", surround_fun("(", ")"), {expr = true})
-inrm("<C-l>", surround_fun("[", "]"), {expr = true})
-inrm("<C-f>", surround_fun("{", "}"), {expr = true})
+local surround_fun_com = function(opening, closing)
+    return function()
+        local byte_index = vim.fn.getcmdpos()
+        local line = vim.fn.getcmdline():sub(byte_index, byte_index)
+        if closing == line then
+            return "<Right>"
+        else
+            return opening .. closing .. "<Left>"
+        end
+    end
+end
+
+inrm("<C-b>", surround_fun_ins("(", ")"), {expr = true})
+inrm("<C-l>", surround_fun_ins("[", "]"), {expr = true})
+inrm("<C-f>", surround_fun_ins("{", "}"), {expr = true})
+
+cnrm("<C-b>", surround_fun_com("(", ")"), {expr = true})
+cnrm("<C-l>", surround_fun_com("[", "]"), {expr = true})
+cnrm("<C-f>", surround_fun_com("{", "}"), {expr = true})
