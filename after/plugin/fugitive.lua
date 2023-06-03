06c827d898e3eee519ac13df889815b2c398e8a7
John Montgomery <j.ace.svg@gmail.com>
2023-06-03 01:06:50 -0500

    [FIX] Prevent Fugitive command line mappings from interfering with :global

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,6 @@
 local Remap = require('j-ace-svg.keymap')
 local nnrm = Remap.nnoremap
+local cnrm = Remap.cnoremap
 local ca = Remap.ca
 
 nnrm('<Leader>gs', ':Git status<CR>', { silent = true })
@@ -16,7 +17,38 @@ nnrm('<Leader>gA', ':Git add % ')
 nnrm('<Leader>gb', ':Git branch ')
 nnrm('<Leader>go', ':Git checkout ')
 
-ca('g', 'Git')
-ca('gd', 'Git diff')
-ca('gls', 'Git ls-files')
-ca('gsh', 'Git stash')
+local git_at_start = function(abbr_list, trigger) -- Only expand Git abbreviation at the start of the line
+    return function()
+        local total_str = ""
+        local byte_index = vim.fn.getcmdpos() - 1
+        for abbreviation, expansion in pairs(abbr_list) do
+            local abbr_len = abbreviation:len()
+            if vim.fn.getcmdline():sub(byte_index - abbr_len, byte_index) == abbreviation and byte_index == abbr_len then
+                return string.rep("<BS>", abbr_len) .. expansion .. trigger
+            end
+            total_str = total_str ..  vim.fn.getcmdline():sub(byte_index, byte_index - abbreviation:len())
+        end
+        return trigger
+    end
+end
+
+local git_abbreviations = {
+    ["g"] = "Git",
+    -- ["ga"] = "Git add",
+    -- ["gA"] = "Git add %",
+    -- ["gb"] = "Git branch",
+    -- ["gc"] = "Git commit",
+    ["gd"] = "Git diff",
+    ["gls"] = "Git ls-files",
+    ["gsh"] = "Git stash",
+    -- ["gp"] = "Git push -u ",
+    -- ["gu"] = "Git pull",
+    -- ["go"] = "Git checkout",
+}
+cnrm(' ', git_at_start(git_abbreviations, ' '), { expr = true })
+cnrm('<C-m>', git_at_start(git_abbreviations, '<C-m>'), { expr = true })
+cnrm('<C-j>', git_at_start(git_abbreviations, '<C-j>'), { expr = true })
+cnrm('<C-t>', git_at_start(git_abbreviations, '<C-m>'), { expr = true })
+-- ca('gd', 'Git diff')
+-- ca('gls', 'Git ls-files')
+-- ca('gsh', 'Git stash')
