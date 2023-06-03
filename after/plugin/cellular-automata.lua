193349832bdd10321d96a1cc841f253b50e0ee09
John Montgomery <j.ace.svg@gmail.com>
2023-06-03 01:21:59 -0500

    [FEAT] Configure Cellular Automata Pluggin

---
+++ after/plugin/cellular-automata.lua
@@ -0,0 +1,25 @@
+local Remap = require('j-ace-svg.keymap')
+local nnrm = Remap.nnoremap
+local cell = require('cellular-automaton')
+local register = cell.register_animation
+local start = cell.start_animation
+
+local slide = {
+    fps = 50,
+    name = 'slide',
+    update = function (grid)
+        for i = 1, #grid do
+            local prev = grid[i][#(grid[i])]
+            for j = 1, #(grid[i]) do
+                grid[i][j], prev = prev, grid[i][j]
+            end
+        end
+        return true
+    end
+}
+
+register(slide)
+
+nnrm("<leader>cam", function() start("make_it_rain") end, {silent = true})
+nnrm("<leader>cal", function() start("game_of_life") end, {silent = true})
+nnrm("<leader>cas", function() start("slide") end, {silent = true})
