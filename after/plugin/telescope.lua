969a44a46243c72ce1d771fa0b6d1d52e3a46130
John Montgomery <j.ace.svg@gmail.com>
2023-08-31 06:56:02 -0500

    [FEAT] Make project search use project directory rather than buffer directory

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,7 +4,7 @@ local builtin = require('telescope.builtin')
 local utils = require('telescope.utils')
 
 nnrm('<Leader>pf', function()
-    builtin.find_files({ hidden = true, respect_gitignore = true, cwd = utils.buffer_dir() });
+    builtin.find_files({ hidden = true, respect_gitignore = true });
 end)
 nnrm('<Leader>pg', function()
     builtin.git_files({ hidden = true, respect_gitignore = true });
