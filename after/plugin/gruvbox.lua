e576a5f9c92f008b1748b14dbb5a5bd44ddd9df3
John Montgomery <j.ace.svg@gmail.com>
2023-09-13 00:20:34 -0500

    [FIX] Italicise italic highlight group

--- after/plugin/gruvbox.lua
+++ after/plugin/gruvbox.lua
@@ -3,4 +3,5 @@ vim.cmd.colorscheme("gruvbox")
 vim.cmd[[
     highlight! link TermCursor Cursor
     highlight! TermCursorNC guibg=red guifg=white ctermbg=1 ctermfg=15
+    highlight! @text.emphasis gui=italic
 ]]
