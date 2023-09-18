5be61785e944d8579f7a4539d1e28054432fc88b
John Montgomery <j.ace.svg@gmail.com>
2023-09-17 21:37:41 -0500

    [FIX] Bolden bold highlight group

--- after/plugin/gruvbox.lua
+++ after/plugin/gruvbox.lua
@@ -4,4 +4,5 @@ vim.cmd[[
     highlight! link TermCursor Cursor
     highlight! TermCursorNC guibg=red guifg=white ctermbg=1 ctermfg=15
     highlight! @text.emphasis gui=italic
+    highlight! @text.strong gui=bold
 ]]
