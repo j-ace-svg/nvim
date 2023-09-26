e3b34a22f660d08bf7c058d57aa876cde511b582
John Montgomery <j.ace.svg@gmail.com>
2023-09-26 00:01:06 -0500

    [REFACTOR] Extract Oil setup opts to Lazy `opts' key

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,19 +1,20 @@
 return {
     'stevearc/oil.nvim',
-    config = function ()
+    opts = {
+        default_file_explorer = false,
+        keymaps = {
+            ["L"] = "actions.select",
+            ["H"] = "actions.parent",
+            ["J"] = "j",
+            ["K"] = "k",
+        }
+    },
+    config = function (opts)
         local Remap = require("j-ace-svg.keymap")
         local nnrm = Remap.nnoremap
         local oil = require("oil")
 
-        oil.setup({
-            default_file_explorer = false,
-            keymaps = {
-                ["L"] = "actions.select",
-                ["H"] = "actions.parent",
-                ["J"] = "j",
-                ["K"] = "k",
-            }
-        })
+        oil.setup(opts)
 
         nnrm("<Leader>e", oil.open)
         nnrm("<Leader>E", "<C-w>v:Oil<CR>", { silent = true })
